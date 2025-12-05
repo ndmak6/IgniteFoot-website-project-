ce4e15e686adf37c48fc2aa41a6299f705a60700
ndmak6 <ndmak6@gmail.com>
2025-12-05 14:21:15 +0700

    Add files via upload

---
+++ model.php
@@ -0,0 +1,21 @@
+<?php
+
+class database {
+    public $conn;
+    public function __construct()
+    {
+        $host = "localhost";
+        $dbname = "igniteFoot";
+        $user = "root";
+        $pass = "";
+
+        try{
+            $this->conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+        }catch(PDOException $e){
+            error_log($e->getMessage());
+            die("kết nối thất bại");        
+        }
+    }
+}
+
+?>
