de4ac6783a2ed285843f0a1441e2d73c450dc3c5
ndmak6 <ndmak6@gmail.com>
2025-12-07 13:05:24 +0700

    chèn template home và shop

--- controllers/controller.php
+++ controllers/controller.php
@@ -8,7 +8,14 @@ class controller {
     }
 
     public function home(){
-        include "./views/header.php";
+        include "./views/main-content-home.php";
+        include "./views/footer.php";
+    }
+
+    public function shop(){
+        $page = "shop";
+        include "./views/header-main-without-home.php";
+        include "./views/main-content-shop.php";
         include "./views/footer.php";
     }
 }
