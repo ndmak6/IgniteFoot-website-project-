13196776f9978cb82b5f8b732ca1e088dfe18bb0
Trungkien06-tn <nghiemkien2006@gmail.com>
2025-12-10 02:29:26 +0700

    update controller + views

--- controllers/controller.php
+++ controllers/controller.php
@@ -14,14 +14,14 @@ class controller {
 
     public function shop(){
         $page = "shop";
+        $products = $this->model->getAll();
         include "./views/header-main-without-home.php";
         include "./views/main-content-shop.php";
         include "./views/footer.php";
     }
-    public function getAllProducts(){
-        $products = $this->model->getAll();
-        include "./views/main-content-shop.php";
-    }
+    // public function getAllProducts(){
+    //     include "./views/main-content-shop.php";
+    // }
     public function shoppingcart(){
         include "./views/shoppingcart.php";
     }
