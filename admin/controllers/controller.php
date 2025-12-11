68c60bca37b8e0d6bc0ca0dd3ba35aa68394ba8a
maquocanh2k4-stack <maquocanh2k4@gmail.com>
2025-12-11 10:05:49 +0700

    udate tiếp quản lý danh mục

--- admin/controllers/controller.php
+++ admin/controllers/controller.php
@@ -85,17 +85,15 @@ public function dashboard(){
 //     include "admin/views/layout.php";
 // }
 public function cateProducts(){
-    // 1. GỌI HÀM LẤY DỮ LIỆU TỪ MODEL
     $categories = $this->modelAdmin->getAllCategories(); 
 
     ob_start();
-    // 2. NHÚNG VIEW ĐÃ CÓ BIẾN $categories
     require "admin/views/cateProducts.php";
     $content = ob_get_clean();
     include "admin/views/layout.php";
 }
 public function addcateProductsF(){
-    include "admin/views/addcateProducts.php";
+    include "admin/views/add_cateProducts.php";
 }
 public function addcateProductsHandle(){
     $ten_dm = $_POST['ten_danh_muc'];
@@ -109,6 +107,31 @@ public function addcateProductsHandle(){
 
     $this->modelAdmin->addCategory($ten_dm, $anh_dai_dien);
     header('location: admin.php?pageAdmin=cateProducts');
-}   
+    }
+public function deleteCategory($id_dm){
+    $id_dm = $_GET['id_dm'];
+    $this->modelAdmin->deleteCategory($id_dm);
+    header("location: admin.php?pageAdmin=cateProducts");
 }
-?>
+public function editCateProductsF(){
+    $id_dm = $_GET['id_dm'];
+    $category = $this->modelAdmin->getcatebyid($id_dm);
+    include "admin/views/edit_cateProducts.php";
+}
+public function editCateProductsHandle(){
+    $id_dm = $_POST['id_danh_muc'];
+    $ten_dm = $_POST['ten_danh_muc'];
+
+    // Xử lý ảnh upload
+    if (!empty($_FILES['anh_dai_dien']['name'])) {
+        $target = "assets/uploads/" . basename($_FILES['anh_dai_dien']['name']);
+        move_uploaded_file($_FILES['anh_dai_dien']['tmp_name'], $target);
+        $anh_dai_dien = $target;
+    } else {
+        $anh_dai_dien = $_POST['anh_cu'];
+    }
+
+    $this->modelAdmin->updateCate($ten_dm, $anh_dai_dien, $id_dm);
+    header("Location: admin.php?pageAdmin=cateProducts");
+}
+}
