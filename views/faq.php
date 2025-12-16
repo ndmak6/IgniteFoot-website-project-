1bd952a226456bbdc8b181cecffbf6c39e0c1a10
Auquoc08122006 <auvanquoc27@gmail.com>
2025-12-16 21:06:54 +0700

    SỬA HEADER FAQ

--- views/faq.php
+++ views/faq.php
@@ -587,8 +587,8 @@
                                 </ul>
                             </li>
                             <li class="menu-item-has-children">
-                                <a href="#" class="drop-down"> PAGES</a><i class="bi bi-plus dropdown-icon"></i>
-                                <ul class="sub-menu">
+                                <a href="index.php?page=faq" class="drop-down">FAQ's</a><i class="bi bi-plus dropdown-icon"></i>
+                                <!-- <ul class="sub-menu">
                                     <li><a href="about-us.html">About Us</a></li>
                                     <li>
                                         <a href="categories.html">Category</a>
@@ -601,7 +601,7 @@
                                     </li>
                                     <li><a href="faq.html">FAQ's</a></li>
                                     <li><a href="error.html">Error</a></li>
-                                </ul>
+                                </ul> -->
                             </li>
                             <li><a href="contact.html">CONTACT</a></li>
                         </ul>
